f20698923daf920484785950b302f6f65a62a368
Manas Das <manaspratimdas2004@gmail.com>
2024-07-05 17:46:55 +0530

    initial commit

---
+++ src/main/java/com/ms/sec_pov/SecPovApplication.java
@@ -0,0 +1,13 @@
+package com.ms.sec_pov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecPovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecPovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/sec_pov/SecPovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.sec_pov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecPovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
